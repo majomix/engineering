6b2e7343bac1f7a9ec4d3e8b2d8f3074e66b5cd0
Mario Csaplar <mario.csaplar@centrum.sk>
2024-07-07 00:57:20 +0200

    ctci 18 find start of cycle

--- LeetCode/Cracking/LinkedLists/Task2_8CycleDetection.cs
+++ LeetCode/Cracking/LinkedLists/Task2_8CycleDetection.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.Serialization.Formatters;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 
 namespace LeetCode.Cracking.LinkedLists
@@ -50,6 +49,83 @@ namespace LeetCode.Cracking.LinkedLists
 
             return false;
         }
+
+        /// <summary>
+        /// Part 2: Given a linked list which might contain a loop, implement an algorithm that returns the node at the beginning of the loop.
+        /// </summary>
+        public CrackingLinkedListNode? GetBeginningOfCycleThroughHashSet(CrackingLinkedListNode? head)
+        {
+            var hashSet = new HashSet<CrackingLinkedListNode>();
+
+            var current = head;
+
+            while (current != null)
+            {
+                if (!hashSet.Add(current))
+                {
+                    return current;
+                }
+
+                current = current.Next;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// For every p steps of tortoise, hare takes 2p steps.
+        /// When tortoise enters the loop after k steps, hare has taken 2k steps since beginning.
+        /// Since hare has taken 2k steps and it took tortoise k steps to reach beginning of the loop, hare must be k steps into the loop (2k - k).
+        /// Since k may be larger than loop size, let's denote that hare has taken K = k % loopSize steps inside the loop.
+        ///
+        /// !!! At the point when tortoise enters the loop, tortoise is at element 0 of the loop and hare is at element K of the loop.
+        /// Thus, tortoise is K steps behind hare and hare is loopSize - K steps behind tortoise.
+        /// Hare catches up to tortoise at pace 1 step per unit of time.
+        ///
+        /// If hare is loopSize - K steps behind tortoise and catches up at rate 1 step per unit of time, they will meet after loopSize - K steps.
+        /// If tortoise is at element 0, after loopSize - K steps both tortoise and hare will be K steps before the element 0.
+        /// This is the collision spot.
+        ///
+        /// Because k = K + m * loopSize for any integer m, it is also correct to say that the collision spot is k steps from the loop start.
+        /// Therefore, both collision spot and head of the linked list are equally k nodes away from the start of the loop.
+        ///
+        /// If we keep one pointer at collision spot, move other pointer to head of the linked list and advance both at the same pace of 1 step,
+        /// they will both meet at the start of the loop.
+        /// </summary>
+        public CrackingLinkedListNode? GetBeginningOfCycleThroughFloyd(CrackingLinkedListNode? head)
+        {
+            var tortoise = head;
+            var hare = head;
+            CrackingLinkedListNode? collisionSpot = null;
+
+            // find collision spot
+            while (hare != null && hare.Next != null)
+            {
+                tortoise = tortoise!.Next;
+                hare = hare.Next.Next;
+
+                if (tortoise == hare)
+                {
+                    collisionSpot = hare;
+                    break;
+                }
+            }
+
+            // no collision spot means no cycle
+            if (collisionSpot == null)
+                return null;
+
+            // reset pointers according to description of the algorithm
+            tortoise = head;
+            hare = collisionSpot;
+            while (tortoise != hare)
+            {
+                tortoise = tortoise!.Next;
+                hare = hare!.Next;
+            }
+
+            return tortoise;
+        }
     }
 
     [TestFixture]
@@ -74,7 +150,7 @@ namespace LeetCode.Cracking.LinkedLists
 
 
         [Test]
-        public void ContainsCycleThroughHashSet_DoesNotContainsCycle()
+        public void ContainsCycleThroughHashSet_DoesNotContainCycle()
         {
             // arrange
             var sut = new Task2_8CycleDetection();
@@ -106,7 +182,7 @@ namespace LeetCode.Cracking.LinkedLists
 
 
         [Test]
-        public void ContainsCycleThroughFloyd_DoesNotContainsCycle()
+        public void ContainsCycleThroughFloyd_DoesNotContainCycle()
         {
             // arrange
             var sut = new Task2_8CycleDetection();
@@ -117,6 +193,68 @@ namespace LeetCode.Cracking.LinkedLists
 
             // assert
             result.Should().BeFalse();
+        }
+
+        [Test]
+        public void GetBeginningOfCycleThroughHashSet_ContainsCycle()
+        {
+            // arrange
+            var sut = new Task2_8CycleDetection();
+            var list = CrackingLinkedListProvider.CreateLinkedListWithContent(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            var nodeFive = list.Next!.Next!.Next!.Next!.Next!;
+            var nodeNine = nodeFive.Next!.Next!.Next!.Next!;
+            nodeNine.Next = nodeFive;
+
+            // act
+            var result = sut.GetBeginningOfCycleThroughHashSet(list);
+
+            // assert
+            result.Should().Be(nodeFive);
+        }
+
+        [Test]
+        public void GetBeginningOfCycleThroughHashSet_DoesNotContainCycle()
+        {
+            // arrange
+            var sut = new Task2_8CycleDetection();
+            var list = CrackingLinkedListProvider.CreateLinkedListWithContent(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+            // act
+            var result = sut.GetBeginningOfCycleThroughHashSet(list);
+
+            // assert
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void GetBeginningOfCycleThroughFloyd_ContainsCycle()
+        {
+            // arrange
+            var sut = new Task2_8CycleDetection();
+            var list = CrackingLinkedListProvider.CreateLinkedListWithContent(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            var nodeFive = list.Next!.Next!.Next!.Next!.Next!;
+            var nodeNine = nodeFive.Next!.Next!.Next!.Next!;
+            nodeNine.Next = nodeFive;
+
+            // act
+            var result = sut.GetBeginningOfCycleThroughFloyd(list);
+
+            // assert
+            result.Should().Be(nodeFive);
+        }
+
+        [Test]
+        public void GetBeginningOfCycleThroughFloyd_DoesNotContainCycle()
+        {
+            // arrange
+            var sut = new Task2_8CycleDetection();
+            var list = CrackingLinkedListProvider.CreateLinkedListWithContent(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+
+            // act
+            var result = sut.GetBeginningOfCycleThroughFloyd(list);
+
+            // assert
+            result.Should().BeNull();
         }
     }
 }
---
+++ LeetCode/Medium/LinkedList_FindKthElementFromEnd.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using LeetCode.Cracking.LinkedLists;
+using NUnit.Framework;
+
+namespace LeetCode.Medium
+{
+    /// <summary>
+    /// Given a linked list, find the k-th element from the end.
+    ///
+    /// Discussion:
+    /// Two passes -> find length in first pass, count elements in the second pass.
+    /// Two passes -> move one pointer k elements ahead and keep advancing both pointers by 1. Once the ahead pointer reaches end, the other is k elements from the end.
+    /// 
+    /// Solutions:
+    /// * two pointers
+    /// </summary>
+    internal class LinkedList_FindKthElementFromEnd
+    {
+        public CrackingLinkedListNode? FindKthElementOfLinkedList(CrackingLinkedListNode? head, int k)
+        {
+            var ahead = head;
+            var behind = head;
+
+            for (var i = 0; i < k; i++)
+            {
+                ahead = ahead?.Next;
+            }
+
+            if (ahead == null)
+                return null;
+
+            while (ahead.Next != null)
+            {
+                ahead = ahead.Next;
+                behind = behind!.Next;
+            }
+
+            return behind;
+        }
+    }
+
+    [TestFixture]
+    public class LinkedList_FindKthElementFromEndTests
+    {
+        private static object[] testCases =
+        {
+            new object[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 2, 7 },
+            new object[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 7, 2 },
+            new object[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 0, 9 },
+            new object[] { new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9, 0 }
+        };
+
+        [TestCaseSource(nameof(testCases))]
+        public void FindKthElementOfLinkedListTests_KthElementInRange(List<int> input, int k, int expectedResult)
+        {
+            // arrange
+            var sut = new LinkedList_FindKthElementFromEnd();
+            var list = CrackingLinkedListProvider.CreateLinkedListWithContent(input);
+
+            // act
+            var result = sut.FindKthElementOfLinkedList(list, k);
+
+            // assert
+            result!.Data.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void FindKthElementOfLinkedListTests_KthElementOutOfRange()
+        {
+            // arrange
+            var sut = new LinkedList_FindKthElementFromEnd();
+            var input = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var kOutOfRange = input.Count;
+            var list = CrackingLinkedListProvider.CreateLinkedListWithContent(input);
+
+            // act
+            var result = sut.FindKthElementOfLinkedList(list, kOutOfRange);
+
+            // assert
+            result.Should().Be(null);
+        }
+    }
+}
