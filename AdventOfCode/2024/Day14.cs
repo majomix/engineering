cc0240f729bb127f88a7f82364ac98636297e32e
Mario Csaplar <mario.csaplar@centrum.sk>
2025-04-03 22:30:56 +0200

    aoc 2024 days 14 and 15 pt1

---
+++ AdventOfCode/2024/Day14.cs
@@ -0,0 +1,247 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace AdventOfCode._2024;
+
+internal static class Day14
+{
+    /// <summary>
+    /// The area outside the bathroom is swarming with robots! To get The Historian safely to the bathroom, you'll need a way to predict where the robots will be in the future.
+    /// Fortunately, they all seem to be moving on the tile floor in predictable straight lines. You make a list of all of the robots' current positions (p)
+    /// and velocities (v), one robot per line.
+    ///
+    /// Each robot's position is given as p=x,y where x represents the number of tiles the robot is from the left wall and y represents the number of tiles from the top wall
+    /// (when viewed from above). So, a position of p=0,0 means the robot is all the way in the top-left corner.
+    ///
+    /// Each robot's velocity is given as v=x,y where x and y are given in tiles per second. Positive x means the robot is moving to the right, and positive y means the robot
+    /// is moving down. So, a velocity of v=1,-2 means that each second, the robot moves 1 tile to the right and 2 tiles up.
+    ///
+    /// The robots outside the actual bathroom are in a space which is 101 tiles wide and 103 tiles tall when viewed from above.
+    /// The robots are good at navigating over/under each other (due to a combination of springs, extendable legs, and quadcopters), so they can share the same tile
+    /// and don't interact with each other.
+    ///
+    /// These robots have a unique feature for maximum bathroom security: they can teleport. When a robot would run into an edge of the space they're in, they instead teleport
+    /// to the other side, effectively wrapping around the edges.
+    ///
+    /// To determine the safest area, count the number of robots in each quadrant after 100 seconds. Robots that are exactly in the middle (horizontally or vertically)
+    /// don't count as being in any quadrant.
+    /// </summary>
+    public static int GetSafetyFactor(string[] input, Point gridSize)
+    {
+        var secondsToSimulate = 100;
+
+        var robots = ParseInput(input);
+        var bathroom = new Bathroom();
+
+        foreach (var robot in robots)
+        {
+            var targetPosition = robot.SimulateMovement(gridSize, secondsToSimulate);
+            bathroom.AddRobot(targetPosition);
+        }
+
+        return bathroom.GetSafetyFactor(gridSize);
+    }
+
+    private static List<BathroomRobot> ParseInput(string[] input)
+    {
+        var result = new List<BathroomRobot>();
+
+        foreach (var line in input)
+        {
+            var split = line.Split(new[] { "p=", " v=", "," }, StringSplitOptions.RemoveEmptyEntries);
+            if (split.Length == 4)
+            {
+                var position = new Point(int.Parse(split[0]), int.Parse(split[1]));
+                var velocity = new Point(int.Parse(split[2]), int.Parse(split[3]));
+                var robot = new BathroomRobot(position, velocity);
+
+                result.Add(robot);
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// During the bathroom break, someone notices that these robots seem awfully similar to ones built and used at the North Pole.
+    /// If they're the same type of robots, they should have a hard-coded Easter egg: very rarely, most of the robots should arrange themselves into a picture
+    /// of a Christmas tree.
+    ///
+    /// What is the fewest number of seconds that must elapse for the robots to display the Easter egg?
+    /// </summary>
+    public static int GetNumberOfSecondsForChristmasTreeArrangement(string[] input, Point gridSize)
+    {
+        var robots = ParseInput(input);
+        var simulationAnalysisResult = 7623;
+
+        var bathroom = new Bathroom();
+
+        foreach (var robot in robots)
+        {
+            var targetPosition = robot.SimulateMovement(gridSize, simulationAnalysisResult);
+            bathroom.AddRobot(targetPosition);
+        }
+
+        bathroom.Print(gridSize, simulationAnalysisResult);
+
+        return simulationAnalysisResult;
+    }
+}
+
+public class BathroomRobot
+{
+    private readonly Point _position;
+    private readonly Point _velocity;
+
+    public BathroomRobot(Point position, Point velocity)
+    {
+        _position = position;
+        _velocity = velocity;
+    }
+
+    public Point SimulateMovement(Point gridSize, int secondsToSimulate)
+    {
+        var targetX = (gridSize.X + ((_position.X + _velocity.X * secondsToSimulate) % gridSize.X)) % gridSize.X;
+        var targetY = (gridSize.Y + ((_position.Y + _velocity.Y * secondsToSimulate) % gridSize.Y)) % gridSize.Y;
+
+        return new Point(targetX, targetY);
+    }
+}
+
+public class Bathroom
+{
+    private class Quadrant
+    {
+        public Point LeftTop { get; }
+        public Point RightBottom { get; }
+
+        public static List<Quadrant> CreateQuadrants(Point gridSize)
+        {
+            var endOfLeftQuadrantX = gridSize.X / 2;
+            var startOfRightQuadrantX = endOfLeftQuadrantX + 1;
+            var endOfUpperQuadrantY = gridSize.Y / 2;
+            var startOfLowerQuadrantY = endOfUpperQuadrantY + 1;
+
+            var quadrants = new List<Quadrant>
+            {
+                new(new Point(0, 0), new Point(endOfLeftQuadrantX, endOfUpperQuadrantY)),
+                new(new Point(startOfRightQuadrantX, 0), new Point(gridSize.X, endOfUpperQuadrantY)),
+                new(new Point(0, startOfLowerQuadrantY), new Point(endOfLeftQuadrantX, gridSize.Y)),
+                new(new Point(startOfRightQuadrantX, startOfLowerQuadrantY), new Point(gridSize.X, gridSize.Y))
+            };
+
+            return quadrants;
+        }
+
+        private Quadrant(Point leftTop, Point rightBottom)
+        {
+            LeftTop = leftTop;
+            RightBottom = rightBottom;
+        }
+    }
+
+    private readonly Dictionary<Point, int> _robots = new();
+
+    public void AddRobot(Point position)
+    {
+        if (!_robots.TryAdd(position, 1))
+        {
+            _robots[position]++;
+        }
+    }
+
+    public int GetSafetyFactor(Point gridSize)
+    {
+        var quadrants = Quadrant.CreateQuadrants(gridSize);
+
+        var safetyFactor = 1;
+
+        foreach (var quadrant in quadrants)
+        {
+            var robotsInQuadrant = 0;
+
+            for (var y = quadrant.LeftTop.Y; y < quadrant.RightBottom.Y; y++)
+            {
+                for (var x = quadrant.LeftTop.X; x < quadrant.RightBottom.X; x++)
+                {
+                    if (_robots.TryGetValue(new Point(x, y), out var robotsInCell))
+                    {
+                        robotsInQuadrant += robotsInCell;
+                    }
+                }
+            }
+
+            if (robotsInQuadrant != 0)
+            {
+                safetyFactor *= robotsInQuadrant;
+            }
+        }
+
+        return safetyFactor;
+    }
+
+    public void Print(Point gridSize, int steps)
+    {
+        Debug.WriteLine($"STEPS: {steps}");
+
+        for (var y = 0; y < gridSize.Y; y++)
+        {
+            for (var x = 0; x < gridSize.X; x++)
+            {
+                if (_robots.TryGetValue(new Point(x, y), out var value))
+                {
+                    Debug.Write(value);
+                }
+                else
+                {
+                    Debug.Write(".");
+                }
+            }
+            Debug.WriteLine(string.Empty);
+        }
+    }
+}
+
+[TestFixture]
+internal class Day14Tests
+{
+    [Test]
+    public void Day14Task1Example()
+    {
+        string[] input =
+        {
+            "p=0,4 v=3,-3",
+            "p=6,3 v=-1,-3",
+            "p=10,3 v=-1,2",
+            "p=2,0 v=2,-1",
+            "p=0,0 v=1,3",
+            "p=3,0 v=-2,-2",
+            "p=7,6 v=-1,-3",
+            "p=3,0 v=-1,-2",
+            "p=9,3 v=2,3",
+            "p=7,3 v=-1,2",
+            "p=2,4 v=2,-3",
+            "p=9,5 v=-3,-3"
+        };
+
+        Day14.GetSafetyFactor(input, new Point(11, 7)).Should().Be(12);
+    }
+
+    [Test]
+    public void Day14Task1()
+    {
+        var input = EmbeddedResourceHelper.GetResourceLines("AdventOfCode._2024.day14.txt");
+        var result = Day14.GetSafetyFactor(input, new Point(101, 103));
+        result.Should().Be(222208000);
+    }
+
+    [Test]
+    public void Day14Task2()
+    {
+        var input = EmbeddedResourceHelper.GetResourceLines("AdventOfCode._2024.day14.txt");
+        var result = Day14.GetNumberOfSecondsForChristmasTreeArrangement(input, new Point(101, 103));
+        result.Should().Be(7623);
+    }
+}
---
+++ AdventOfCode/2024/Day15.cs
@@ -0,0 +1,245 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode._2024;
+
+internal static class Day15
+{
+    /// <summary>
+    /// The lanternfish already have a map of the warehouse and a list of movements the robot will attempt to make. The problem is that the movements will sometimes fail
+    /// as boxes are shifted around, making the actual movements of the robot difficult to predict.
+    ///
+    /// As the robot (@) attempts to move, if there are any boxes (O) in the way, the robot will also attempt to push those boxes.
+    /// However, if this action would cause the robot or a box to move into a wall (#), nothing moves instead, including the robot.
+    /// The initial positions of these are shown on the map at the top of the document the lanternfish gave you.
+    ///
+    /// The rest of the document describes the moves that the robot will attempt to make, in order.
+    ///
+    /// The lanternfish use their own custom Goods Positioning System (GPS for short) to track the locations of the boxes. The GPS coordinate of a box is equal
+    /// to 100 times its distance from the top edge of the map plus its distance from the left edge of the map.
+    /// This process does not stop at wall tiles; measure all the way to the edges of the map.
+    ///
+    /// The lanternfish would like to know the sum of all boxes' GPS coordinates after the robot finishes moving.
+    /// Predict the motion of the robot and boxes in the warehouse.
+    /// 
+    /// After the robot is finished moving, what is the sum of all boxes' GPS coordinates?
+    /// </summary>
+    public static int GetSumOfAllBoxesGps(string[] input)
+    {
+        var sokoban = new LanternfishSokoban(input);
+        sokoban.Play();
+        var result = sokoban.GetSumOfGpsCoordinates();
+
+        return result;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    public static int Task2(string[] input)
+    {
+        var sokoban = new ExpandedLanternfishSokoban(input);
+    }
+}
+
+public class ExpandedLanternfishSokoban
+{
+
+}
+
+public class LanternfishSokoban
+{
+    private readonly Queue<char> _playerMoves = new();
+    private readonly HashSet<Point> _walls = new();
+    private readonly HashSet<Point> _boxes = new();
+
+    private Point _player;
+
+    public LanternfishSokoban(string[] input)
+    {
+        var parsingLevel = true;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var line = input[i];
+
+            if (parsingLevel)
+            {
+                if (string.Empty.Equals(line))
+                {
+                    parsingLevel = false;
+                }
+                else
+                {
+                    for (var j = 0; j < line.Length; j++)
+                    {
+                        var cell = line[j];
+                        var cellLocation = new Point(j, i);
+                        switch (cell)
+                        {
+                            case '#':
+                                _walls.Add(cellLocation);
+                                break;
+                            case 'O':
+                                _boxes.Add(cellLocation);
+                                break;
+                            case '@':
+                                _player = cellLocation;
+                                break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var ch in line)
+                {
+                    _playerMoves.Enqueue(ch);
+                }
+            }
+        }
+    }
+
+    public void Play()
+    {
+        foreach (var move in _playerMoves)
+        {
+            _player = TryMove(_player, move);
+        }
+    }
+
+    public int GetSumOfGpsCoordinates()
+    {
+        var sum = 0;
+
+        foreach (var box in _boxes)
+        {
+            sum += box.Y * 100 + box.X;
+        }
+
+        return sum;
+    }
+
+    private Point TryMove(Point currentPosition, char move)
+    {
+        var targetPosition = GetTargetPosition(currentPosition, move);
+
+        if (_walls.Contains(targetPosition))
+            return currentPosition;
+
+        if (!_boxes.Contains(targetPosition))
+            return targetPosition;
+
+        var targetPositionOfMovedBox = TryMove(targetPosition, move);
+        
+        if (targetPositionOfMovedBox != targetPosition)
+        {
+            _boxes.Remove(targetPosition);
+            _boxes.Add(targetPositionOfMovedBox);
+
+            return targetPosition;
+        }
+
+        return currentPosition;
+    }
+    
+    protected Point GetTargetPosition(Point currentPosition, char move)
+    {
+        switch (move)
+        {
+            case '<':
+                return currentPosition with { X = currentPosition.X - 1 };
+            case '^':
+                return currentPosition with { Y = currentPosition.Y - 1 };
+            case '>':
+                return currentPosition with { X = currentPosition.X + 1 };
+            case 'v':
+                return currentPosition with { Y = currentPosition.Y + 1 };
+        }
+
+        throw new InvalidDataException("No such move known.");
+    }
+}
+
+[TestFixture]
+internal class Day15Tests
+{
+    [Test]
+    public void Day15Task1Example1()
+    {
+        string[] input =
+        {
+            "########",
+            "#..O.O.#",
+            "##@.O..#",
+            "#...O..#",
+            "#.#.O..#",
+            "#...O..#",
+            "#......#",
+            "########",
+            "",
+            "<^^>>>vv<v>>v<<"
+        };
+
+        Day15.GetSumOfAllBoxesGps(input).Should().Be(2028);
+    }
+
+    [Test]
+    public void Day15Task1Example2()
+    {
+        string[] input =
+        {
+            "##########",
+            "#..O..O.O#",
+            "#......O.#",
+            "#.OO..O.O#",
+            "#..O@..O.#",
+            "#O#..O...#",
+            "#O..O..O.#",
+            "#.OO.O.OO#",
+            "#....O...#",
+            "##########",
+            "",
+            "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^",
+            "vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v",
+            "><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<",
+            "<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^",
+            "^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><",
+            "^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^",
+            ">^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^",
+            "<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>",
+            "^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>",
+            "v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"
+        };
+
+        Day15.GetSumOfAllBoxesGps(input).Should().Be(10092);
+    }
+
+    [Test]
+    public void Day15Task1()
+    {
+        var input = EmbeddedResourceHelper.GetResourceLines("AdventOfCode._2024.day15.txt");
+        var result = Day15.GetSumOfAllBoxesGps(input);
+        result.Should().Be(1517819);
+    }
+
+    [Test]
+    public void Day15Task2Example()
+    {
+        string[] input =
+        {
+
+        };
+
+        Day15.Task2(input).Should().Be(0);
+    }
+
+    [Test]
+    public void Day15Task2()
+    {
+        var input = EmbeddedResourceHelper.GetResourceLines("AdventOfCode._2024.day15.txt");
+        var result = Day15.Task2(input);
+        result.Should().Be(0);
+    }
+}
